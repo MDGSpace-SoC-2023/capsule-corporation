7a871cedfc2a556a355c9ef20caa5dbb1868ee5a
Kartik <agarwalkartik2018@gmail.com>
2023-12-27 22:40:35 +0530

    Remove all files

--- middlewares/csrf-token.js
+++
@@ -1,7 +0,0 @@
-function addCsrfToken(req, res, next) {
-  res.locals.csrfToken = req.csrfToken();
-  console.log("csrftoken"+res.locals.csrfToken);
-  next();
-}
-
-module.exports = addCsrfToken;
--- middlewares/error-handler.js
+++
@@ -1,6 +0,0 @@
-function handleError(err, req, res, next) {
-    console.log(err);
-    res.status(500).send('Something broke!');
-}
-
-module.exports = handleError;
